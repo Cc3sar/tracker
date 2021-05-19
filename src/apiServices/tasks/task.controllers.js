2fe97bef560a1e77644e5bb627b1984533e96b29
César Perén <cesarjosue12.200@gmail.com>
2021-05-18 22:01:49 -0600

    Se solucionaron errores

--- src/apiServices/tasks/task.controllers.js
+++ src/apiServices/tasks/task.controllers.js
@@ -26,20 +26,24 @@ const showUserTasks = async (req, res) => {
     } = req.query;
 
     const count = await Task.find({ userId: { $in: req.params._id } }).count();
-
-    if(!from || !to || !limit) {
-        const user = req.body.userTask;
-        let showData = await Task.find({ userId: { $in: req.params._id } }, { _id: 0, userId: 0 });
-    } else {
-        showData = await Task.find({ $and: [{ date:{ $gte: new Date(from), $lte: new Date(to) } } ] }, { _id: 0, userId: 0 }).limit(limit);
+    const user = req.body.userTask;
+    let showData;
+    try {
+        if(!from || !to || !limit) {
+            showData = await Task.find({ userId: { $in: req.params._id } }, { _id: 0, userId: 0 });
+        } else {
+            showData = await Task.find({ $and: [{ date:{ $gte: new Date(from), $lte: new Date(to) } } ] }, { _id: 0, userId: 0 }).limit(limit);
+        }
+        
+        res.json({
+            _id: user._id,
+            username: user.username,
+            count,
+            log: showData
+        });
+    } catch (error) {
+        console.log(error);
     }
-    
-    res.json({
-        _id: user._id,
-        username: user.username,
-        count,
-        log: showData
-    });
 };
 
 const showSpecificUserTasks = async(req, res) => {
